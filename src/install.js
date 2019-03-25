41faf87f2eb215041dbedd20fc04c649067d0f3c
nothingismagick <35242872+nothingismagick@users.noreply.github.com>
2019-03-25 15:22:34 +0100

    return JSDoc stub

--- src/install.js
+++ src/install.js
@@ -4,6 +4,9 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
 
+/**	
+ * @param {function} api	
+ */
 module.exports = function (api) {
 	console.log('NOT READY FOR USE')
 	api.render('./templates/base')
