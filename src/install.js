42f2e142a46115e4825054c55dacb40bc514d1df
Scott <smolinari@users.noreply.github.com>
2019-03-25 15:27:59 +0100

    Initial commit (#1)
    * Initial commit
    
    * Update uninstall.js
    
    * return JSDoc stub

--- src/install.js
+++ src/install.js
@@ -4,9 +4,8 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
 
-/**
- * @param {function} api
- * @returns {boolean}
+/**	
+ * @param {function} api	
  */
 module.exports = function (api) {
 	console.log('NOT READY FOR USE')
--- src/prompts.js
+++ src/prompts.js
@@ -5,34 +5,6 @@
  * (answers are available as "api.prompts" in the other scripts)
  * https://www.npmjs.com/package/inquirer#question
  *
- * Example:
-
-  return [
-    name: {
-      type: 'string',
-      required: true,
-      message: 'Quasar CLI Extension name (without prefix)',
-    },
-    preset: {
-      type: 'checkbox',
-      message: 'Check the features needed for your project:',
-      choices: [
-        {
-          name: 'Install script',
-          value: 'install'
-        },
-        {
-          name: 'Prompts script',
-          value: 'prompts'
-        },
-        {
-          name: 'Uninstall script',
-          value: 'uninstall'
-        }
-      ]
-    }
-  ]
-
  */
 
 module.exports = function () {
