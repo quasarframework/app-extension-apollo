305503d669414be3e5917ba98c332901ab5f4a1a
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2023-09-15 20:34:13 +0300

    feat: add web sockets support

--- src/templates/src/apollo/index.ts
+++ src/templates/src/apollo/index.ts
@@ -1,19 +1,59 @@
 import type { ApolloClientOptions } from '@apollo/client/core'
-import { createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createHttpLink, InMemoryCache, split } from '@apollo/client/core'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities'
 import type { BootFileParams } from '@quasar/app'
+import { createClient } from 'graphql-ws'
 
 export /* async */ function getClientOptions(
   /* {app, router, ...} */ options?: Partial<BootFileParams<any>>
 ) {
+  const httpLink = createHttpLink({
+    uri:
+      process.env.GRAPHQL_URI ||
+      // Change to your graphql endpoint.
+      '/graphql',
+  })
+
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url:
+        process.env.GRAPHQL_URI_WS ||
+        // Change to your graphql endpoint.
+        `ws://${location.host}/graphql`,
+      // If you have authentication, you can utilize connectionParams:
+      /*
+      connectionParams: () => {
+        const session = getSession(); // Change to your way of getting the session.
+        if (!session) {
+          return {};
+        }
+
+        return {
+          Authorization: `Bearer ${session.token}`,
+        };
+      },
+      */
+    })
+  )
+
+  const link = split(
+    // split based on operation type
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      )
+    },
+    wsLink,
+    httpLink
+  )
+
   return <ApolloClientOptions<unknown>>Object.assign(
     // General options.
     <ApolloClientOptions<unknown>>{
-      link: createHttpLink({
-        uri:
-          process.env.GRAPHQL_URI ||
-          // Change to your graphql endpoint.
-          'https://example.com/graphql',
-      }),
+      link,
 
       cache: new InMemoryCache(),
     },
